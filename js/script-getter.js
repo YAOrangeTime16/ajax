4964ddb17313b65c307baaf60ce18324a537aaa6
YAOrangeTime16 <yokoandrae@gmail.com>
2017-03-31 14:40:20 +0200

    modified

--- js/script-getter.js
+++ js/script-getter.js
@@ -1,4 +1,3 @@
-//API "Deck of Card": 'https://deckofcardsapi.com/'
 $(function(){    
     let cardInfoObject;
     let cardArray;
@@ -8,21 +7,23 @@ $(function(){
     let click;
     let num=5;   //for click limit
     
-    //API
-    $('#open').on('click', function(){
-     $('.loading1').fadeIn('slow').delay(500).fadeOut('slow');
-    }).on('click', function(){
-        $.ajax({
-                method: 'GET',
-                dataType: 'json',
-                url: 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
+    //AJAX call
+    $('#open')
+        .on('click', function(){
+            $('.loading1').fadeIn('slow').delay(500).fadeOut('slow');
         })
-        .then(getCards)
-        .then(showCards)
-        .then(addPoints)
-        .then(showFinalScore)
-        .fail(err=>err);
-    })
+        .on('click', function(){
+            $.ajax({
+                    method: 'GET',
+                    dataType: 'json',
+                    url: 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
+            })
+            .then(getCards)
+            .then(showCards)
+            .then(addPoints)
+            .then(showFinalScore)
+            .fail(err=>err);
+        })
         //limit to 5 clikcs
         .on('click', function(){
                 $(this).data('click', --num);
@@ -41,7 +42,7 @@ $(function(){
     })
     
     //FUNCTION DEFINITIONS
-    let getCards=(json)=>{
+    let getCards=json=>{
         cardInfoObject=json.cards;
         cardOne=cardInfoObject[0];
         cardTwo=cardInfoObject[1];
@@ -50,8 +51,7 @@ $(function(){
     
     let showCards=()=>{
         let cardHtml=card=>{
-            return `
-            <figure class="figure-getter">
+            return `<figure class="figure-getter">
                     <img class="card-img" src=${card.image} alt="${card.code}">
                     <figcaption>
                         <p class="label label-default">${card.suit}</p>
@@ -61,10 +61,10 @@ $(function(){
         };
         let firstCard=cardHtml(cardOne);
         let secondCard=cardHtml(cardTwo);
-        $('#cardTable-getter').hide().html(firstCard + secondCard).delay(1500).fadeIn('slow');
+        $('#cardTable-getter').hide().html(firstCard+secondCard).delay(1500).fadeIn('slow');
     };
         
-    let addPoints = ()=>{
+    let addPoints=()=>{
         if(cardOne.value===cardTwo.value){
             points+=30;
             $('#message').hide().delay(1500).fadeIn('slow')
@@ -81,7 +81,7 @@ $(function(){
         return points;
     };
     
-    let showFinalScore=(score)=>{
+    let showFinalScore=score=>{
        if(click===0){  
             $('#counter').hide().html(score).delay(3500).fadeIn('slow');
             $('#message, #cardTable-getter').delay(500).fadeOut('slow');
